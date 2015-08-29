b9c8de8daf517c55e12ae694c7b38a848ddaea50
Josef Bustamante <kfbustam@ucsc.edu>
2015-08-29 16:57:22 -0700

    Map markers (dining halls added)

--- SlugRoute/MapViewController.swift
+++ SlugRoute/MapViewController.swift
@@ -25,18 +25,52 @@ class MapViewController: UIViewController {
         var McHenrymarker = GMSMarker()
         McHenrymarker.position = CLLocationCoordinate2DMake(36.99578157522153, -122.058908423001)
         McHenrymarker.title = "McHenry Library"
+        McHenrymarker.icon = UIImage(named: "library")
         McHenrymarker.snippet = "UCSC"
         McHenrymarker.map = mapView
         var SciEngmarker = GMSMarker()
         SciEngmarker.position = CLLocationCoordinate2DMake(36.99904411574191,-122.06070818525006)
         SciEngmarker.title = "S&E Library"
+        SciEngmarker.icon = UIImage(named: "library")
         SciEngmarker.snippet = "UCSC"
         SciEngmarker.map = mapView
         var c8andOakesDiningHall = GMSMarker()
+        c8andOakesDiningHall.snippet = "UCSC"
         c8andOakesDiningHall.position = CLLocationCoordinate2DMake(36.991655, -122.065103)
         c8andOakesDiningHall.title = "College 8 & Oakes Dining Hall"
-        c8andOakesDiningHall.snippet = "UCSC"
+        c8andOakesDiningHall.appearAnimation = kGMSMarkerAnimationPop
+        c8andOakesDiningHall.icon = UIImage(named: "dining_hall")
         c8andOakesDiningHall.map = mapView
+        var diningPorterKresge = GMSMarker()
+        diningPorterKresge.position = CLLocationCoordinate2DMake(36.994341, -122.066066)
+        diningPorterKresge.snippet = "UCSC"
+        diningPorterKresge.title = "Porter & Kresge Dining Hall"
+        diningPorterKresge.appearAnimation = kGMSMarkerAnimationPop
+        diningPorterKresge.icon = UIImage(named: "dining_hall")
+        diningPorterKresge.map = mapView
+        var diningnineandten = GMSMarker()
+        diningnineandten.position = CLLocationCoordinate2DMake(37.001096, -122.058031)
+        diningnineandten.snippet = "UCSC"
+        diningnineandten.title = "College Nine and Ten Dining Hall"
+        diningnineandten.appearAnimation = kGMSMarkerAnimationPop
+        diningnineandten.icon = UIImage(named: "dining_hall")
+        diningnineandten.map = mapView
+        var diningcrownandmerrill = GMSMarker()
+        diningcrownandmerrill.position = CLLocationCoordinate2DMake(36.999971, -122.054448)
+        diningcrownandmerrill.snippet = "UCSC"
+        diningcrownandmerrill.title = "Crown and Merrill Dining Hall"
+        diningcrownandmerrill.appearAnimation = kGMSMarkerAnimationPop
+        diningcrownandmerrill.icon = UIImage(named: "dining_hall")
+        diningcrownandmerrill.map = mapView
+        var diningcowellandstevenson = GMSMarker()
+        diningcowellandstevenson.position = CLLocationCoordinate2DMake(36.997157, -122.053150)
+        diningcowellandstevenson.snippet = "UCSC"
+        diningcowellandstevenson.title = "Cowell and Stevenson Dining Hall"
+        diningcowellandstevenson.appearAnimation = kGMSMarkerAnimationPop
+        diningcowellandstevenson.icon = UIImage(named: "dining_hall")
+        diningcowellandstevenson.map = mapView
+        
+        
 
         //Must be somewhere in the view! License Info.
         copyright.text = GMSServices.openSourceLicenseInfo()
